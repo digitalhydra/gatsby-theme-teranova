b319c92e430efba50e99ad8d38aa926ffcb20b04
Jairo Rondon <jimnoname@gmail.com>
2019-08-22 11:41:06 -0500

    setup theme

--- gatsby-theme-teranova/src/components/container.js
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-const Container = styled.div`
-  padding: 0 ${props => props.theme.spacing[2]}rem;
-  max-width: 1200px;
-  margin: 0 auto;
-`;
-
-export default Container;
